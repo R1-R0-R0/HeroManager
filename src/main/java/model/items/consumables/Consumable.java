2ff753e4432834b3e1cb1d9c398299a694c74c52
Gf <guillaume13.nicolai@gmail.com>
2021-04-22 16:01:19 +0200

    Correct on consummable

--- src/main/java/model/items/consumables/Consumable.java
+++ src/main/java/model/items/consumables/Consumable.java
@@ -5,18 +5,28 @@ import model.items.Item;
 public class Consumable implements Item {
 
     private ConsumableEffect effect;
+    private String name;
+    private String description;
 
-    public Consumable(ConsumableEffect effect) {
+    public Consumable(ConsumableEffect effect, String name, String description) {
         this.effect = effect;
+        this.name = name;
+        this.description = description;
+    }
+
+    public ConsumableEffect getEffect() {
+        return effect;
     }
 
     @Override
     public String getName() {
-        return null;
+        return name;
     }
 
     @Override
     public String getDescription() {
-        return null;
+        return description;
     }
 }
+
+
