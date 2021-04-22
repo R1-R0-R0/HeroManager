064adca029a10f58379ace2e53d454996b0f43b0
Gf <guillaume13.nicolai@gmail.com>
2021-04-22 11:54:19 +0200

    Add HandsEquipment.java

---
+++ src/main/java/model/items/equipments/HandsEquipment.java
@@ -0,0 +1,36 @@
+package model.items.equipments;
+
+public class HandsEquipment implements Equipment{
+
+
+    String name;
+    String description;
+    int armorBonus;
+    EquipmentEffect effect;
+    EquipmentType type;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public int getArmorBonus() {
+        return armorBonus;
+    }
+
+    @Override
+    public EquipmentEffect getEquipmentEffect() {
+        return effect;
+    }
+
+    @Override
+    public EquipmentType getEquipmentType() {
+        return type;
+    }
+}
