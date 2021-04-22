002cb10399a322f03aa4204707ac45a185ab6e9d
Gf <guillaume13.nicolai@gmail.com>
2021-04-22 11:58:48 +0200

    Add AmuletEquipment.java

---
+++ src/main/java/model/items/equipments/AmuletEquipment.java
@@ -0,0 +1,35 @@
+package model.items.equipments;
+
+public class AmuletEquipment implements Equipment{
+
+    String name;
+    String description;
+    int armorBonus;
+    EquipmentEffect effect;
+    EquipmentType type;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public int getArmorBonus() {
+        return armorBonus;
+    }
+
+    @Override
+    public EquipmentEffect getEquipmentEffect() {
+        return effect;
+    }
+
+    @Override
+    public EquipmentType getEquipmentType() {
+        return type;
+    }
+}
