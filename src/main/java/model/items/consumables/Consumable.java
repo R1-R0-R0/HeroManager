2baa8ee75134749b954e783f531505182e200d9f
Airels <33286314+Airels@users.noreply.github.com>
2021-04-22 11:36:13 +0200

    Created Consumable class

---
+++ src/main/java/model/items/consumables/Consumable.java
@@ -0,0 +1,18 @@
+package model.items.consumables;
+
+import model.items.Item;
+
+public class Consumable implements Item {
+
+    private ConsumableEffect effect;
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+}
