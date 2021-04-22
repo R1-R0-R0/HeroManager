876768b79db6dacc4174284e6afef21c4e92d2e5
Airels <33286314+Airels@users.noreply.github.com>
2021-04-22 11:39:14 +0200

    Created Dice class

---
+++ src/main/java/model/Dice.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Dice {
+    private int diceRange;
+
+    public Dice(int diceRange) {
+        this.diceRange = diceRange;
+    }
+
+    public int getDiceRange() {
+        return diceRange;
+    }
+
+    public int roll() {
+        // TODO : Random from 1 to diceRange
+        return 0;
+    }
+}
