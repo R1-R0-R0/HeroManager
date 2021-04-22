c246cb41dc7addc9f9de38aa25450af84a636157
Gf <guillaume13.nicolai@gmail.com>
2021-04-22 13:02:34 +0200

    Uncommented lines

--- src/main/java/model/job/Job.java
+++ src/main/java/model/job/Job.java
@@ -1,6 +1,7 @@
 package model.job;
 
 import model.items.Item;
+import model.spell.Spell;
 
 import java.util.List;
 
@@ -23,11 +24,9 @@ public class Job {
     private List<Item> inventory;
     private String name, description;
     private int[] spellSlots; // TODO : A modifier
+    private List<Spell> spellInventory;
+    private List<Improvement> improvements;
+    private List<JobSkill> skills;
 
-    /*
-    private List<Spell> spellInventory
-    private List<Improvement> improvements
-    private List<JobSkill> skills
-    */
 
 }
