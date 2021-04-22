c60974af19bc12f78503815510580237a36af888
Gf <guillaume13.nicolai@gmail.com>
2021-04-22 12:23:40 +0200

    Add Improvement

---
+++ src/main/java/model/job/Improvement.java
@@ -0,0 +1,16 @@
+package model.job;
+
+public class Improvement {
+
+    String  name,
+            description;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
