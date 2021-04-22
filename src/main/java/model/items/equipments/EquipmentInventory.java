565e90044522ba9d2355d5cedd281e216c9a4803
Gf <guillaume13.nicolai@gmail.com>
2021-04-22 12:27:46 +0200

    Add EquipmentInventory with getters

---
+++ src/main/java/model/items/equipments/EquipmentInventory.java
@@ -0,0 +1,54 @@
+package model.items.equipments;
+
+public class EquipmentInventory {
+    HeadEquipment head;
+    BodyEquipment body;
+    LegsEquipment legs;
+    FeetsEquipment feets;
+    HandsEquipment hands;
+    BeltEquipment belt;
+    MantleEquipment mantle;
+    AmuletEquipment amulet;
+    RingEquipment leftRing;
+    RingEquipment rightRing;
+
+    public HeadEquipment getHead() {
+        return head;
+    }
+
+    public BodyEquipment getBody() {
+        return body;
+    }
+
+    public LegsEquipment getLegs() {
+        return legs;
+    }
+
+    public FeetsEquipment getFeets() {
+        return feets;
+    }
+
+    public HandsEquipment getHands() {
+        return hands;
+    }
+
+    public BeltEquipment getBelt() {
+        return belt;
+    }
+
+    public MantleEquipment getMantle() {
+        return mantle;
+    }
+
+    public AmuletEquipment getAmulet() {
+        return amulet;
+    }
+
+    public RingEquipment getLeftRing() {
+        return leftRing;
+    }
+
+    public RingEquipment getRightRing() {
+        return rightRing;
+    }
+}
