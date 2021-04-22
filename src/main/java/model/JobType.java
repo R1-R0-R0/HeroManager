3ecba9f4f8499502fe5888ee23cc5c199c8399d5
Airels <33286314+Airels@users.noreply.github.com>
2021-04-22 11:19:52 +0200

    added types of Jobs

---
+++ src/main/java/model/JobType.java
@@ -0,0 +1,16 @@
+package model;
+
+public enum JobType {
+    BARBARIAN,
+    BARD,
+    CLERIC,
+    DRUID,
+    FIGHTER,
+    MONK,
+    PALADIN,
+    RANGER,
+    ROGUE,
+    SORCERER,
+    WARLOCK,
+    WIZARD
+}
