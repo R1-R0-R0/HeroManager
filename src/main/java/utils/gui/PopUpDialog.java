bce1a80175115df0369fd300b384e81b086058aa
Airels <33286314+Airels@users.noreply.github.com>
2021-04-23 18:05:01 +0200

    Implemented dialog

--- src/main/java/utils/gui/PopUpDialog.java
+++ src/main/java/utils/gui/PopUpDialog.java
@@ -1,16 +1,21 @@
 package utils.gui;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
 public class PopUpDialog {
 
     private Alert alert;
 
-    public PopUpDialog(Alert.AlertType alertType) {
+    public PopUpDialog(Alert.AlertType alertType, String title, String text) {
         alert = new Alert(alertType);
-        alert.setTitle("Title");
-        alert.setHeaderText("Header");
-        alert.setContentText("Context");
+        alert.setTitle("HeroManager - Error");
+        alert.setHeaderText(title);
+        alert.setContentText(text);
+    }
+
+    public void addButton(ButtonType buttonType) {
+        alert.getButtonTypes().add(buttonType);
     }
 
     public void show() {
