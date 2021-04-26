04b396d37473ca2228092f7fea5e1f639a970059
SAymeric <aymeric.sibiak@etu.univ-amu.fr>
2021-04-26 10:57:17 +0200

    writer weapon et equipment

--- src/main/java/model/Utils/Writer.java
+++ src/main/java/model/Utils/Writer.java
@@ -1,6 +1,8 @@
 package model.Utils;
 
 import model.items.consumables.Consumable;
+import model.items.equipments.Equipment;
+import model.items.weapons.Weapon;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
@@ -24,7 +26,6 @@ public class Writer {
 
         }
 
-        //Write JSON file
         try (FileWriter file = new FileWriter("/HeroManager/resources/consumable.json")) {
             file.write(consumableList.toJSONString());
             file.flush();
@@ -33,4 +34,56 @@ public class Writer {
             e.printStackTrace();
         }
     }
+
+    public static void WriterWeapon (List<Weapon> weapons){
+        JSONArray weaponList = new JSONArray();
+
+        for (int index = 0; index<weapons.size(); index++){
+
+            JSONObject consu = new JSONObject();
+            consu.put("name" , weapons.get(index).getName());
+            consu.put("description" , weapons.get(index).getDescription());
+            consu.put("type" , weapons.get(index).getWeaponType());
+            consu.put("properties" , weapons.get(index).getProperties());
+            consu.put("damagetype" , weapons.get(index).getDamageType());
+
+            weaponList.add(consu);
+
+        }
+
+        try (FileWriter file = new FileWriter("/HeroManager/resources/weapon.json")) {
+            file.write(weaponList.toJSONString());
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void WriterEquipment (List<Equipment> equipements){
+        JSONArray equipmentList = new JSONArray();
+
+        for (int index = 0; index<equipements.size(); index++){
+
+            JSONObject consu = new JSONObject();
+            consu.put("name" , equipements.get(index).getName());
+            consu.put("description" , equipements.get(index).getDescription());
+            consu.put("armorbonus" , equipements.get(index).getArmorBonus());
+            consu.put("effect" , equipements.get(index).getEquipmentEffect());
+            consu.put("type" , equipements.get(index).getEquipmentType();
+
+            equipmentList.add(consu);
+
+        }
+
+        try (FileWriter file = new FileWriter("/HeroManager/resources/equipment.json")) {
+            file.write(equipmentList.toJSONString());
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
