915057dde7f9274a8c3c38585528128381e80b46
Airels <33286314+Airels@users.noreply.github.com>
2021-04-27 16:05:06 +0200

    Implemented simple text before selection

--- src/main/java/controller/NewGameController.java
+++ src/main/java/controller/NewGameController.java
@@ -6,6 +6,8 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,11 +21,12 @@ public class NewGameController implements Initializable {
     @FXML
     public ImageView imageJobClass;
     @FXML
-    public TextField textJobDesc;
+    public TextFlow textJobDesc;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         imageJob.setImage(new Image(getClass().getResourceAsStream("/images/ui/question_mark.png")));
         imageJobClass.setImage(new Image(getClass().getResourceAsStream("/images/ui/question_mark.png")));
+        textJobDesc.getChildren().add(new Text("<- Select a character on the left."));
     }
 }
