fffa78895a4539f84e7397d027e6962b7f58d9c0
Gf <guillaume13.nicolai@gmail.com>
2021-04-27 12:55:39 +0200

    Add the possibility that Equipment give Characteristics Boosts

--- src/main/java/model/items/equipments/BeltEquipment.java
+++ src/main/java/model/items/equipments/BeltEquipment.java
@@ -2,18 +2,30 @@ package model.items.equipments;
 
 public class BeltEquipment implements Equipment{
 
-    private String name;
-    private String description;
-    private int armorBonus;
-    private EquipmentEffect effect;
-    private EquipmentType type;
+    private final String name;
+    private final String description;
+    private final int armorBonus;
+    private final EquipmentEffect effect;
+    private final EquipmentType type;
+    private final int[] characteristicsBoost = new int[7];
 
-    public BeltEquipment(String name, String description, int armorBonus, EquipmentEffect effect, EquipmentType type) {
+
+    public BeltEquipment(String name, String description, int armorBonus, EquipmentEffect effect, EquipmentType type,
+                           int strengthBoost, int dexterityBoost, int robustnessBoost, int intelligenceBoost, int wisdomBoost,
+                           int charismaBoost, int speedBoost) {
         this.name = name;
         this.description = description;
         this.armorBonus = armorBonus;
         this.effect = effect;
         this.type = type;
+
+        characteristicsBoost[0] = strengthBoost;
+        characteristicsBoost[1] = dexterityBoost;
+        characteristicsBoost[2] = robustnessBoost;
+        characteristicsBoost[3] = intelligenceBoost;
+        characteristicsBoost[4] = wisdomBoost;
+        characteristicsBoost[5] = charismaBoost;
+        characteristicsBoost[6] = speedBoost;
     }
 
     @Override
@@ -39,5 +51,33 @@ public class BeltEquipment implements Equipment{
     @Override
     public EquipmentType getEquipmentType() {
         return type;
+    }
+
+    public int getStrengthBoost(){
+        return characteristicsBoost[0];
+    }
+
+    public int getDexterityBoost(){
+        return characteristicsBoost[1];
+    }
+
+    public int getRobustnessBoost(){
+        return characteristicsBoost[2];
+    }
+
+    public int getIntelligenceBoost(){
+        return characteristicsBoost[3];
+    }
+
+    public int getWisdomBoost(){
+        return characteristicsBoost[4];
+    }
+
+    public int getCharismaBoost(){
+        return characteristicsBoost[5];
+    }
+
+    public int getSpeedBoost(){
+        return characteristicsBoost[6];
     }
 }
