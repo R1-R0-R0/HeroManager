61b8a4c8fac012a52d4835a432cb97bac6b456a8
Airels <33286314+Airels@users.noreply.github.com>
2021-04-28 19:45:37 +0200

    Created item_manager.fxml interface

--- src/main/java/controller/ItemManagerController.java
+++ src/main/java/controller/ItemManagerController.java
@@ -5,12 +5,13 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
+import model.gui.ItemManagerModel;
 import model.items.Item;
 import model.items.ItemType;
+import model.items.weapons.DamageType;
+import model.items.weapons.WeaponType;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,11 +24,27 @@ public class ItemManagerController implements Initializable {
     public ListView<Item> itemList;
     @FXML
     public Label itemNameLabel;
+    @FXML
+    public TextField nameText;
+    @FXML
+    public TextArea descriptionText;
+    @FXML
+    public TextArea propertiesText;
+    @FXML
+    public ChoiceBox<WeaponType> weaponTypePicker;
+    @FXML
+    public ChoiceBox<DamageType> damageTypePicker;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        ObservableList<ItemType> list = FXCollections.observableArrayList(ItemType.values());
-        typePicker.setItems(list);
+        ObservableList<ItemType> itemTypeObservableList = FXCollections.observableArrayList(ItemType.values());
+        typePicker.setItems(itemTypeObservableList);
+
+        ObservableList<WeaponType> weaponTypeObservableList = FXCollections.observableArrayList(WeaponType.values());
+        weaponTypePicker.setItems(weaponTypeObservableList);
+
+        ObservableList<DamageType> damageTypeObservableList = FXCollections.observableArrayList(DamageType.values());
+        damageTypePicker.setItems(damageTypeObservableList);
     }
 
     @FXML
@@ -39,5 +56,25 @@ public class ItemManagerController implements Initializable {
             case EQUIPMENTS:
             case CONSUMABLES:
         }
+    }
+
+    @FXML
+    public void newItemButtonOnClick() {
+
+    }
+
+    @FXML
+    public void updateItemButtonOnClick() {
+
+    }
+
+    @FXML
+    public void deleteItemButtonOnClick() {
+
+    }
+
+    @FXML
+    public void backButtonOnClick() {
+        ItemManagerModel.getInstance().returnToMenu();
     }
 }
