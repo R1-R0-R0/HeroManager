d3313f50170ebc1bbba6306f8d4260fc458ae0c0
Airels <33286314+Airels@users.noreply.github.com>
2021-05-05 19:44:42 +0200

    Implemented full character creation test

--- src/test/java/gui/CharacterCreatorTest.java
+++ src/test/java/gui/CharacterCreatorTest.java
@@ -3,10 +3,15 @@ package gui;
 import controller.CharacterCreatorController;
 import javafx.application.Application;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.Spinner;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import model.gui.MenuModel;
+import model.job.JobSkill;
+import model.job.JobType;
+import model.spell.Component;
+import model.spell.Spell;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,6 +19,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
+import utils.gui.character_creator.JobSkillItem;
+import utils.gui.character_creator.SpellItem;
+import view.CharacterCreatorView;
+import view.CharacterView;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @ExtendWith(ApplicationExtension.class)
 public class CharacterCreatorTest extends Application {
@@ -183,5 +196,52 @@ public class CharacterCreatorTest extends Application {
 
         robot.clickOn("#createCharacterButton");
         Assertions.assertFalse(CharacterCreatorController.getInstance().spellsTab.isDisabled());
+    }
+
+    @Test
+    @DisplayName("GUI Character creation test fully character creation")
+    public void fullCharacterCreationTest(FxRobot robot) throws InterruptedException {
+        Spell fireBall = new Spell("Fireball", "Launch a ball of fire dealing magical damages", "Fire", "1 turn",
+                "no duration", 5, 35, JobType.WIZARD, true,
+                Collections.singletonList(Component.VOCAL));
+        Spell frostSpike = new Spell("Frostspike", "Launch a spike of ice dealing magical movement impairment", "Ice", "instant",
+                "3 turns", 6, 40, JobType.SORCERER, false,
+                Arrays.asList(Component.VOCAL, Component.MATERIAL));
+        List<Spell> spells = Arrays.asList(fireBall, frostSpike);
+
+        List<JobSkill> skills = Arrays.asList();
+
+        createWarlock(robot);
+
+        CharacterCreatorView.getInstance().setSpellsListView(spells);
+
+        robot.clickOn("#createCharacterButton");
+        robot.clickOn("#spinnerStrength");
+
+        for (int i = 0; i < 6; i++) {
+            robot.type(KeyCode.RIGHT, KeyCode.RIGHT, KeyCode.TAB);
+        }
+
+        robot.clickOn("#skillsTab");
+        Assertions.assertEquals(skills.size(), ((ListView<JobSkillItem>) robot.lookup("#skillsListView").tryQuery().get()).getItems().size());
+        if (skills.size() > 0) {
+            robot.type(KeyCode.TAB);
+
+            for (int i = 0; i < skills.size(); i++)
+                robot.type(KeyCode.TAB, KeyCode.ENTER);
+        }
+
+        robot.clickOn("#spellsTab");
+        Assertions.assertEquals(spells.size(), ((ListView<SpellItem>) robot.lookup("#spellsListView").tryQuery().get()).getItems().size());
+        if (spells.size() > 0) {
+            robot.type(KeyCode.TAB);
+
+            for (int i = 0; i < spells.size(); i++)
+                robot.type(KeyCode.TAB, KeyCode.ENTER);
+        }
+
+
+        robot.clickOn("#launchGameButton");
+        Assertions.assertTrue(CharacterView.getInstance().getStage().isShowing());
     }
 }
