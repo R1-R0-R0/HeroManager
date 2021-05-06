ca9c64dff508714b48bfe297bcf946a2c8648d1d
ASibiak <aymericsibiak@gmail.com>
2021-05-06 15:32:29 +0200

    javadoc DefaultFileContent

--- src/main/java/utils/files/DefaultFileContent.java
+++ src/main/java/utils/files/DefaultFileContent.java
@@ -1,17 +1,31 @@
 package utils.files;
 
+/**
+ * Enum to create a new File when the reader can't find the searched file
+ * In case if the user deleted the the save file.
+ */
 public enum DefaultFileContent {
     WEAPONS("[machin, bidule, chouette]");
     //SPEELS("[machin, bidule, chouette]");
     //CONSUMABLE("[machin, bidule, chouette]");
     //EQUIPMENT("");
-
+    /**
+     * Instance of this class
+     */
     private String content;
 
+    /**
+     * Set a default content
+     * @param content needed string
+     */
     DefaultFileContent(String content) {
         this.content = content;
     }
 
+    /**
+     * Return a Enum to a string
+     * @return String
+     */
     @Override
     public String toString() {
         return content;
