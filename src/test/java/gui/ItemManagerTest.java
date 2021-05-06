398d024a057326da33d30885ef5b374180a0ca01
Airels <33286314+Airels@users.noreply.github.com>
2021-05-06 14:40:52 +0200

    Created test to check if list view is correctly updated

--- src/test/java/gui/ItemManagerTest.java
+++ src/test/java/gui/ItemManagerTest.java
@@ -1,9 +1,18 @@
 package gui;
 
 import controller.ItemManagerController;
+import javafx.scene.control.ListView;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import model.gui.ItemManagerModel;
+import model.items.Item;
+import model.items.consumables.Consumable;
+import model.items.equipments.Equipment;
+import model.items.equipments.EquipmentType;
+import model.items.equipments.HeadEquipment;
+import model.items.weapons.DamageType;
+import model.items.weapons.Weapon;
+import model.items.weapons.WeaponType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,7 +21,13 @@ import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 @ExtendWith(ApplicationExtension.class)
+@DisplayName("GUI Item Manager Tests")
 public class ItemManagerTest {
 
     @Start
@@ -21,8 +36,8 @@ public class ItemManagerTest {
     }
 
     @Test
-    @DisplayName("GUI Item Manager test if Combo Box (Picker) and corresponding pane will be visible correctly for each type")
-    public void typePickerPaneVisibilityTest(FxRobot robot) throws InterruptedException {
+    @DisplayName("Test if Combo Box (Picker) and corresponding pane will be visible correctly for each type")
+    public void typePickerPaneVisibilityTest(FxRobot robot) {
         Assertions.assertFalse(ItemManagerController.getInstance().weaponsPane.isVisible());
         Assertions.assertFalse(ItemManagerController.getInstance().equipmentPane.isVisible());
         Assertions.assertFalse(ItemManagerController.getInstance().consumablePane.isVisible());
@@ -47,5 +62,48 @@ public class ItemManagerTest {
         Assertions.assertFalse(ItemManagerController.getInstance().weaponsPane.isVisible());
         Assertions.assertFalse(ItemManagerController.getInstance().equipmentPane.isVisible());
         Assertions.assertTrue(ItemManagerController.getInstance().consumablePane.isVisible());
+    }
+
+    @Test
+    @DisplayName("Test if list views contains goods values")
+    public void listViewTest(FxRobot robot) {
+        ItemManagerModel model = ItemManagerModel.getInstance();
+        model.setWeaponList(Collections.singletonList(new Weapon("Axe", "An axe", "She's sharp", WeaponType.COMMON, DamageType.SLASHING)));
+        model.setEquipmentList(Collections.singletonList(new HeadEquipment("Headphones", "A simple pair of headphones, useless in fight", 0, null, EquipmentType.LIGHT, 0, 0, 0, 0, 0, 1, 0)));
+        model.setConsumableList(Arrays.asList(new Consumable("A potion", "Unknown potion. Will you drink it ?"), new Consumable("Gameboy", "A simple gameboy.")));
+
+        ListView<Item> listView = ((ListView<Item>) robot.lookup("#itemList").tryQuery().get());
+
+        Assertions.assertEquals(0, listView.getItems().size());
+
+        robot.clickOn("#typePicker");
+        robot.type(KeyCode.DOWN);
+        Assertions.assertEquals(1, listView.getItems().size());
+        Assertions.assertEquals("Axe", listView.getItems().get(0).getName());
+
+        robot.type(KeyCode.DOWN);
+        Assertions.assertEquals(1, listView.getItems().size());
+        Assertions.assertEquals("Headphones", listView.getItems().get(0).getName());
+
+        robot.type(KeyCode.DOWN);
+        Assertions.assertEquals(2, listView.getItems().size());
+        Assertions.assertEquals("Gameboy", listView.getItems().get(1).getName());
+    }
+
+    @Test
+    @DisplayName("Test if buttons are enabled/disabled correctly")
+    public void buttonsEnabledTest(FxRobot robot) {
+        ItemManagerController controller = ItemManagerController.getInstance();
+
+        Assertions.assertTrue(controller.newItemButton.isDisabled());
+        Assertions.assertTrue(controller.updateItemButton.isDisabled());
+        Assertions.assertTrue(controller.deleteItemButton.isDisabled());
+
+        robot.clickOn("#typePicker");
+        robot.type(KeyCode.DOWN);
+
+        Assertions.assertFalse(controller.newItemButton.isDisabled());
+        Assertions.assertTrue(controller.updateItemButton.isDisabled());
+        Assertions.assertTrue(controller.deleteItemButton.isDisabled());
     }
 }
