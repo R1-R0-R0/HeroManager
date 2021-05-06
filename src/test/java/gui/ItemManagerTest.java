2f34fc151a5f2c9d63bd47fa92dc5f44bbb1991f
Airels <33286314+Airels@users.noreply.github.com>
2021-05-06 13:17:16 +0200

    Implemented new tests to check type picker and panes visibility works correctly

--- src/test/java/gui/ItemManagerTest.java
+++ src/test/java/gui/ItemManagerTest.java
@@ -1,11 +1,14 @@
 package gui;
 
 import controller.ItemManagerController;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import model.gui.ItemManagerModel;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
@@ -18,9 +21,31 @@ public class ItemManagerTest {
     }
 
     @Test
-    public void test() throws InterruptedException {
+    @DisplayName("GUI Item Manager test if Combo Box (Picker) and corresponding pane will be visible correctly for each type")
+    public void typePickerPaneVisibilityTest(FxRobot robot) throws InterruptedException {
         Assertions.assertFalse(ItemManagerController.getInstance().weaponsPane.isVisible());
         Assertions.assertFalse(ItemManagerController.getInstance().equipmentPane.isVisible());
         Assertions.assertFalse(ItemManagerController.getInstance().consumablePane.isVisible());
+
+        robot.clickOn("#typePicker");
+        robot.type(KeyCode.DOWN);
+
+        Assertions.assertTrue(ItemManagerController.getInstance().weaponsPane.isVisible());
+        Assertions.assertFalse(ItemManagerController.getInstance().equipmentPane.isVisible());
+        Assertions.assertFalse(ItemManagerController.getInstance().consumablePane.isVisible());
+
+        robot.clickOn("#typePicker");
+        robot.type(KeyCode.DOWN);
+
+        Assertions.assertFalse(ItemManagerController.getInstance().weaponsPane.isVisible());
+        Assertions.assertTrue(ItemManagerController.getInstance().equipmentPane.isVisible());
+        Assertions.assertFalse(ItemManagerController.getInstance().consumablePane.isVisible());
+
+        robot.clickOn("#typePicker");
+        robot.type(KeyCode.DOWN);
+
+        Assertions.assertFalse(ItemManagerController.getInstance().weaponsPane.isVisible());
+        Assertions.assertFalse(ItemManagerController.getInstance().equipmentPane.isVisible());
+        Assertions.assertTrue(ItemManagerController.getInstance().consumablePane.isVisible());
     }
 }
