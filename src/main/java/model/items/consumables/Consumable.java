49cb80541d07455809e3618a7d6af80f1a36f50f
Gf <guillaume13.nicolai@gmail.com>
2021-05-06 16:52:03 +0200

    added javaDoc + delete ConsumableEffect

--- src/main/java/model/items/consumables/Consumable.java
+++ src/main/java/model/items/consumables/Consumable.java
@@ -2,21 +2,32 @@ package model.items.consumables;
 
 import model.items.Item;
 
+/**
+ * Creation of consumable items
+ */
 public class Consumable implements Item {
 
     private String name;
     private String description;
 
-    public Consumable(ConsumableEffect effect, String name, String description) {
+    public Consumable(String name, String description) {
         this.name = name;
         this.description = description;
     }
 
+    /**
+     *
+     * @return name of the consumable
+     */
     @Override
     public String getName() {
         return name;
     }
 
+    /**
+     *
+     * @return description of the consumable
+     */
     @Override
     public String getDescription() {
         return description;
