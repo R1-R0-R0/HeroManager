217affd6dd13ac2dd16eaa50504025d6becbe1e2
Gf <guillaume13.nicolai@gmail.com>
2021-05-07 14:02:48 +0200

    Modification of enum Jobtype to specify Dlife and improvements

--- src/main/java/model/job/JobType.java
+++ src/main/java/model/job/JobType.java
@@ -1,19 +1,46 @@
 package model.job;
 
+import model.Characteristics;
+import model.items.equipments.Equipment;
+import model.items.equipments.EquipmentType;
+import model.items.weapons.WeaponType;
+
+import javax.print.attribute.standard.MediaSize;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Lists all the existing JobType
  */
 public enum JobType {
-    BARBARIAN,
-    BARD,
-    CLERIC,
-    DRUID,
-    FIGHTER,
-    MONK,
-    PALADIN,
-    RANGER,
-    ROGUE,
-    SORCERER,
-    WARLOCK,
-    WIZARD
+    BARBARIAN(12,Improvement.RAGE,Improvement.UNARMORED_DEFENSE),
+    BARD(8,Improvement.BARDIC_INSPIRATION), //add spellCasting
+    CLERIC(8, Improvement.DIVINE_DOMAIN), //add spellCasting
+    DRUID(8,Improvement.DRUIDIC), // add spellCasting
+    FIGHTER(10,Improvement.FIGHTING_STYLE,Improvement.SECOND_WIND),
+    MONK(8,Improvement.UNARMORED_DEFENSE, Improvement.MARTIAL_ARTS),
+    PALADIN(10,Improvement.DIVINE_SENSE,Improvement.LAY_ON_HANDS),
+    RANGER(10,Improvement.FAVORED_ENEMY,Improvement.NATURAL_EXPLORER),
+    ROGUE(8,Improvement.EXPERTISE,Improvement.SNEAK_ATTACK,Improvement.THIEVES_CANT),
+    SORCERER(6,Improvement.SORCEROUS_ORIGIN),  //add spellCasting
+    WARLOCK(8, Improvement.OTHERWORDLY_PATRON, Improvement.PACT_MAGIC),
+    WIZARD(6,Improvement.ARCANE_RECOVERY); // add spellCasting
+
+
+    protected int dLife;
+    protected Improvement[] improvements;
+
+    JobType(int dLife,Improvement... improvements){
+        this.dLife = dLife;
+        this.improvements = improvements;
+
+    }
+
+    public List<Improvement> getImprovements() {
+        return Arrays.asList(improvements);
+    }
+
+    public int getDLife() {
+        return dLife;
+    }
 }
