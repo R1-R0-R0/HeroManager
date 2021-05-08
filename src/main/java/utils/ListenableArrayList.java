726680e24b3738d4c7f35e8880888f4dd26b0239
Airels <33286314+Airels@users.noreply.github.com>
2021-05-08 17:25:30 +0200

    Created listenable array list

---
+++ src/main/java/utils/ListenableArrayList.java
@@ -0,0 +1,11 @@
+package utils;
+
+import java.util.ArrayList;
+
+/**
+ * An array list class where we can add listeners on modification methods
+ */
+public class ListenableArrayList<T> extends ArrayList<T> {
+
+}
+
