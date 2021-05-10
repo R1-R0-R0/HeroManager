1924f87b30a67fe1c21f4c5d964e2f846495bf47
Airels <33286314+Airels@users.noreply.github.com>
2021-05-10 19:24:09 +0200

    Added result return to showAndWait Dialog

--- src/main/java/utils/gui/Dialog.java
+++ src/main/java/utils/gui/Dialog.java
@@ -1,10 +1,11 @@
 package utils.gui;
 
+import javafx.collections.ObservableList;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
-import utils.SimpleListener;
 
 import java.awt.*;
+import java.util.Optional;
 
 /**
  * Utility class usable to show simple dialog alert
@@ -12,6 +13,7 @@ import java.awt.*;
 public class Dialog {
 
     private Alert alert;
+    private Optional<ButtonType> result;
 
     /**
      * Constructor of this class, who allows to init desired dialog
@@ -24,15 +26,14 @@ public class Dialog {
         alert.setTitle("HeroManager - " + alertType.name().toLowerCase());
         alert.setHeaderText(title);
         alert.setContentText(text);
-
-
     }
 
-    public Dialog(String title, String text, SimpleListener answerListener) {
-        alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Hero Manager - confirmation");
-        alert.setHeaderText(title);
-        alert.setContentText(text);
+    /**
+     * Used to get buttons linked to dialog
+     * @return observable list of buttons of dialog
+     */
+    public ObservableList<ButtonType> getButtons() {
+        return alert.getButtonTypes();
     }
 
     /**
@@ -45,10 +46,11 @@ public class Dialog {
 
     /**
      * To show dialog
+     * @return result of dialog
      */
-    public void showAndWait() {
+    public Optional<ButtonType> showAndWait() {
         playSound();
-        alert.showAndWait();
+        return alert.showAndWait();
     }
 
     /**
