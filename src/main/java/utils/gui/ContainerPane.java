c792b6b98641b50ff8559c8f8f98188d2b111c62
Airels <33286314+Airels@users.noreply.github.com>
2021-05-10 17:13:14 +0200

    Created extension of StackPane that can contains T object

---
+++ src/main/java/utils/gui/ContainerPane.java
@@ -0,0 +1,62 @@
+package utils.gui;
+
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+
+/**
+ * A JavaFX StackPane extension that can contains and give an object
+ *
+ * @param <T> type of object
+ */
+public class ContainerPane<T> extends StackPane {
+
+    /**
+     * The contained T object
+     */
+    private T containedObject;
+
+    /**
+     * Empty contructor to create an instance without default contained object
+     */
+    public ContainerPane() {
+
+    }
+
+    /**
+     * Contructor taking a T object to set it up at start
+     *
+     * @param object object to contain
+     */
+    public ContainerPane(T object) {
+        containedObject = object;
+        super.getChildren().add(new Text(object.toString()));
+    }
+
+    /**
+     * To retrieve contained object already assigned. May return null object.
+     *
+     * @return contained object or null if doesn't exist
+     */
+    public T getContainedObject() {
+        return containedObject;
+    }
+
+    /**
+     * To set contained object and set his toString String in a text passed to super StackPane extended
+     *
+     * @param containedObject object to contain
+     */
+    public void setContainedObject(T containedObject) {
+        this.containedObject = containedObject;
+        super.getChildren().clear();
+        super.getChildren().add(new Text(containedObject.toString()));
+    }
+
+    /**
+     * To set contained object to null and clear text previously passed to super StackPane extended
+     */
+    public void clearContainedObject() {
+        containedObject = null;
+        super.getChildren().clear();
+    }
+}
