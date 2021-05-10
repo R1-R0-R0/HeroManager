251799e7a1ac3baf5a4cc4417d377eb66093ee81
Airels <33286314+Airels@users.noreply.github.com>
2021-05-10 15:28:13 +0200

    Implemented all setters related to showing/clearing items info

--- src/main/java/view/ItemPickerView.java
+++ src/main/java/view/ItemPickerView.java
@@ -112,4 +112,120 @@ public class ItemPickerView {
     public void setListView(List<? extends Item> items) {
         ItemPickerController.getInstance().itemList.setItems(FXCollections.observableArrayList(items));
     }
+
+    /**
+     * Show information about given item
+     * @param item to show information about
+     * @throws UnsupportedItemException if type of selected item isn't recognized
+     */
+    public void showItemInfo(Item item) throws UnsupportedItemException {
+        ItemPickerView.getInstance().clearItemInfo();
+        if (item == null) {
+            ItemPickerView.getInstance().setSelectItemButtonDisable(true);
+            return;
+        }
+
+        ItemPickerView.getInstance().setSelectItemButtonDisable(false);
+
+        if (item instanceof Weapon)
+            showWeaponInfo(((Weapon) item));
+        else if (item instanceof Equipment)
+            showEquipmentInfo(((Equipment) item));
+        else if (item instanceof Consumable)
+            showConsumableInfo(((Consumable) item));
+        else
+            throw new UnsupportedItemException("Unsupported item " + item);
+
+        ItemPickerController.getInstance().itemNameLabel.setText(item.getName());
+    }
+
+    /**
+     * Clear all information shown about an item
+     */
+    public void clearItemInfo() {
+        ItemPickerController controller = ItemPickerController.getInstance();
+        controller.itemNameLabel.setText("");
+        controller.itemInfoPane.getChildren().clear();
+    }
+
+    /**
+     * Show information about a weapon, with its unique item properties
+     * @param weapon to show information about
+     */
+    private void showWeaponInfo(Weapon weapon) {
+        ObservableList<Node> info = ItemPickerController.getInstance().itemInfoPane.getChildren();
+
+        Text mainInfo = new Text(
+                "Weapon type: " + weapon.getWeaponType() +
+                        "\nDamage type inflicted: " + weapon.getDamageType()
+        );
+
+        Text properties = new Text(
+                "\n\nProperties: " + weapon.getProperties()
+        );
+
+        Text desc = new Text(
+                "\n\n---\n\n" + weapon.getDescription()
+        );
+
+        info.add(new TextFlow(mainInfo, properties, desc));
+    }
+
+    /**
+     * Show information about an equipment, with its unique item properties
+     * @param equipment to show information about
+     */
+    private void showEquipmentInfo(Equipment equipment) {
+        ObservableList<Node> info = ItemPickerController.getInstance().itemInfoPane.getChildren();
+
+        Text mainInfo = new Text(
+                "Can be worn on: " + equipment.getEquipmentParts() +
+                        "\nEquipment type: " + equipment.getEquipmentType()
+        );
+
+        Text stats = new Text(
+                "\n\nStatistics:" +
+                        "\n\t- Armor Bonus:\t\t" + equipment.getArmorBonus() +
+                        "\n\t- Strength Boost:\t\t" + equipment.getStrengthBoost() +
+                        "\n\t- Dexterity Boost:\t\t" + equipment.getDexterityBoost() +
+                        "\n\t- Robustness Boost:\t\t" + equipment.getRobustnessBoost() +
+                        "\n\t- Intelligence Boost:\t\t" + equipment.getIntelligenceBoost() +
+                        "\n\t- Wisdom Boost:\t\t" + equipment.getWisdomBoost() +
+                        "\n\t- Charisma Boost:\t\t" + equipment.getCharismaBoost() +
+                        "\n\t- Speed Boost:\t\t\t" + equipment.getSpeedBoost()
+        );
+
+        Text desc = new Text(
+                "\n\n---\n\n" + equipment.getDescription()
+        );
+
+
+        info.add(new TextFlow(mainInfo, stats, desc));
+    }
+
+    /**
+     * Show information about a consumable, with its unique item properties
+     * @param consumable to show information about
+     */
+    private void showConsumableInfo(Consumable consumable) {
+        ItemPickerController.getInstance().itemInfoPane.getChildren().add(
+                new TextFlow(new Text(consumable.getDescription()))
+        );
+    }
+
+    /**
+     * Enable or disable select button
+     * @param disabled TRUE to disable it, FALSE otherwise
+     */
+    public void setSelectItemButtonDisable(boolean disabled) {
+        ItemPickerController.getInstance().selectItemButton.setDisable(disabled);
+    }
+
+    /**
+     *
+     * @return stage of view
+     */
+    public Stage getStage() {
+        return stage;
+    }
 }
