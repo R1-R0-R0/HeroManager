440889707ce048fd4c2c2bb969fae55e988a7c8c
Airels <33286314+Airels@users.noreply.github.com>
2021-05-10 15:19:27 +0200

    Created tests for ItemPicker view with specific item type

--- src/test/java/model/gui/item_picker/ItemPickerFixedType.java
+++ src/test/java/model/gui/item_picker/ItemPickerFixedType.java
@@ -1,10 +1,11 @@
-package model.gui;
+package model.gui.item_picker;
 
 import javafx.scene.control.ListView;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
-import jdk.jshell.spi.ExecutionControl;
+import model.gui.ItemPickerModel;
 import model.items.Item;
+import model.items.ItemType;
 import model.items.consumables.Consumable;
 import model.items.equipments.Equipment;
 import model.items.equipments.EquipmentParts;
@@ -24,8 +25,8 @@ import java.util.Arrays;
 import java.util.Collections;
 
 @ExtendWith(ApplicationExtension.class)
-@DisplayName("Item Picker view tests")
-class ItemPickerTest {
+@DisplayName("Item Picker view tests for a fixed value")
+public class ItemPickerFixedType {
 
     private static final Weapon weapon = new Weapon("Axe", "An axe", "She's sharp", WeaponType.COMMON, DamageType.SLASHING);
     private static final Equipment equipment = new Equipment("Headphones", "A simple pair of headphones, useless in fight", EquipmentParts.HEAD, 11, EquipmentType.LIGHT, 22, 33, 44, 55, 66, 77, 88);
@@ -35,30 +36,21 @@ class ItemPickerTest {
 
     @Start
     public void start(Stage primaryStage) {
-        ItemPickerModel model = new ItemPickerModel(primaryStage);
+        ItemPickerModel model = new ItemPickerModel(primaryStage, ItemType.CONSUMABLES);
         model.setWeaponList(Collections.singletonList(weapon));
         model.setEquipmentList(Collections.singletonList(equipment));
         model.setConsumableList(Arrays.asList(consumable1, consumable2));
     }
 
     @Test
-    @DisplayName("Check if selected type in picker show corrects values")
+    @DisplayName("Check type picker disable status")
     public void typePickerTest(FxRobot robot) {
-        Assertions.assertFalse(robot.lookup("#typePicker").tryQuery().get().isDisabled());
+        Assertions.assertTrue(robot.lookup("#typePicker").tryQuery().get().isDisable());
+    }
 
-        robot.clickOn("#typePicker");
-        robot.type(KeyCode.DOWN, KeyCode.ENTER);
-
-        Assertions.assertEquals(1, ((ListView<Item>) robot.lookup("#itemList").tryQuery().get()).getItems().size());
-
-        robot.clickOn("#typePicker");
-        robot.type(KeyCode.DOWN, KeyCode.ENTER);
-
-        Assertions.assertEquals(1, ((ListView<Item>) robot.lookup("#itemList").tryQuery().get()).getItems().size());
-
-        robot.clickOn("#typePicker");
-        robot.type(KeyCode.DOWN, KeyCode.ENTER);
-
+    @Test
+    @DisplayName("Check if list view already show imposed items")
+    public void itemListPresetTest(FxRobot robot) {
         Assertions.assertEquals(2, ((ListView<Item>) robot.lookup("#itemList").tryQuery().get()).getItems().size());
     }
 }
