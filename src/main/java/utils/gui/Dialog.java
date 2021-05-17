e76a0ca2cc35987c46221df0b856fc863e30d967
Airels <33286314+Airels@users.noreply.github.com>
2021-05-17 21:18:10 +0200

    Created exception dialog handler

--- src/main/java/utils/gui/Dialog.java
+++ src/main/java/utils/gui/Dialog.java
@@ -1,10 +1,16 @@
 package utils.gui;
 
 import javafx.collections.ObservableList;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
+import javafx.event.EventHandler;
+import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 
 import java.awt.*;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Optional;
 
 /**
@@ -14,6 +20,7 @@ public class Dialog {
 
     private Alert alert;
     private Optional<ButtonType> result;
+    private boolean exceptionAlert;
 
     /**
      * Constructor of this class, who allows to init desired dialog
@@ -26,6 +33,46 @@ public class Dialog {
         alert.setTitle("HeroManager - " + alertType.name().toLowerCase());
         alert.setHeaderText(title);
         alert.setContentText(text);
+
+        exceptionAlert = false;
+    }
+
+    /**
+     * 2nd constructor, used to show an error pop up with an exception.
+     * Print stack trace in standard output, and end program
+     * @param title title of the dialog
+     * @param e exception to show
+     */
+    public Dialog(String title, Exception e) {
+        alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("HeroManager - Critical error");
+        alert.setHeaderText(title);
+
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        String exceptionText = sw.toString();
+
+        Label label = new Label("Exception stacktrace :");
+
+        TextArea exceptionTextArea = new TextArea(exceptionText);
+        exceptionTextArea.setEditable(false);
+        exceptionTextArea.setWrapText(true);
+        exceptionTextArea.setMaxWidth(Double.MAX_VALUE);
+        exceptionTextArea.setMaxHeight(Double.MAX_VALUE);
+        GridPane.setVgrow(exceptionTextArea, Priority.ALWAYS);
+        GridPane.setHgrow(exceptionTextArea, Priority.ALWAYS);
+
+        GridPane expContent = new GridPane();
+        expContent.setMaxWidth(Double.MAX_VALUE);
+        expContent.add(label, 0, 0);
+        expContent.add(exceptionTextArea, 0, 1);
+
+        alert.getDialogPane().setExpandableContent(expContent);
+
+        e.printStackTrace();
+
+        exceptionAlert = true;
     }
 
     /**
@@ -42,6 +89,9 @@ public class Dialog {
     public void show() {
         playSound();
         alert.show();
+
+        if (exceptionAlert)
+            alert.setOnCloseRequest(event -> System.exit(1));
     }
 
     /**
@@ -50,7 +100,13 @@ public class Dialog {
      */
     public Optional<ButtonType> showAndWait() {
         playSound();
-        return alert.showAndWait();
+
+        if (exceptionAlert) {
+            alert.showAndWait();
+            System.exit(1);
+            return Optional.empty();
+        } else
+            return alert.showAndWait();
     }
 
     /**
