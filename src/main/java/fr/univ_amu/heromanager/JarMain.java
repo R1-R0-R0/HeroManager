894e3433c76a73f26ad785404c33c4b89c439db6
Airels <33286314+Airels@users.noreply.github.com>
2021-05-20 14:06:08 +0200

    javadoc correction

--- src/main/java/fr/univ_amu/heromanager/JarMain.java
+++ src/main/java/fr/univ_amu/heromanager/JarMain.java
@@ -1,6 +1,14 @@
 package fr.univ_amu.heromanager;
 
+/**
+ * Entry of HeroManager app if using a jar bundle to init JavaFX components
+ */
 public class JarMain {
+
+    /**
+     * Main entry of program if jar bundle used, to init JavaFX components
+     * @param args
+     */
     public static void main(String[] args) {
         Main.main(args);
     }
